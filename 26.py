1ce776e6214e78770c3e2c177b2f14a123964205
maab2198 <maab2198@gmail.com>
2020-04-01 14:23:45 +0300

    31_march: tack number 50, easy arrs

---
+++ 26.py
@@ -0,0 +1,10 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        n = len(nums)
+        j = 0
+        for i in range(1,n):
+            if nums[i] != nums[j]:
+                j +=1
+                nums[j] = nums[i]
+        print(nums)
+        return j+1
