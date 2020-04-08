ab6ececaa98e922a987f5793b2d92ad423897429
maab2198 <maab2198@gmail.com>
2020-04-08 20:39:15 +0300

    8_april: 1385,1390 repeat

--- march/12/1385.py
+++ march/12/1385.py
@@ -1,11 +1,21 @@
+# class Solution:
+#     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
+#         counter = 0
+#         for el1 in arr1:
+#             for el2 in arr2:
+#                 if abs(el1 - el2) <= d:
+#                     counter +=1
+#                     break
+#         return len(arr1) - counter
+
+# faster with n logn idea
 class Solution:
     def findTheDistanceValue(self, arr1: List[int], arr2: List[int], d: int) -> int:
-        counter = 0
-        for el1 in arr1:
-            for el2 in arr2:
-                if abs(el1 - el2) <= d:
-                    counter +=1
-                    break
-        return len(arr1) - counter
-
-        
+        arr2.sort()
+        count = len(arr1)
+        n = len(arr2)
+        for el in arr1:
+            mid = bisect.bisect(arr2,el)
+            if (0<=mid<n and abs(arr2[mid]-el)<=d) or (0<=mid-1<n and abs(arr2[mid-1]-el)<=d):
+                count-=1
+        return count
--- march/13/1390.py
+++ march/13/1390.py
@@ -1,20 +1,37 @@
+# class Solution:
+#     def sumFourDivisors(self, nums: List[int]) -> int:
+#         s = 0
+#         for el in nums:
+#             divs_arr = self.printDivisors(el)
+#             if len(divs_arr) == 4:
+#                 s+=sum(divs_arr)
+#         return s
+#     def printDivisors(self,n) :
+#         arr = []
+#         i = 1
+#         while i <= math.sqrt(n): 
+#             if (n % i == 0) : 
+#                 if (n / i == i) : 
+#                     arr.append(i), 
+#                 else : 
+#                     arr.append(i)
+#                     arr.append(int(n/i)) 
+#             i = i + 1
+#         return(arr)
+
+### faster
 class Solution:
     def sumFourDivisors(self, nums: List[int]) -> int:
-        s = 0
-        for el in nums:
-            divs_arr = self.printDivisors(el)
-            if len(divs_arr) == 4:
-                s+=sum(divs_arr)
-        return s
-    def printDivisors(self,n) :
-        arr = []
-        i = 1
-        while i <= math.sqrt(n): 
-            if (n % i == 0) : 
-                if (n / i == i) : 
-                    arr.append(i), 
-                else : 
-                    arr.append(i)
-                    arr.append(int(n/i)) 
-            i = i + 1
-        return(arr)
+        res = 0
+        for num in nums:
+            divisor = set([1,num])
+            for i in range(2, floor(sqrt(num)) + 1):
+                if num % i == 0:
+                    divisor.add(num//i)
+                    divisor.add(i)
+                if len(divisor) > 4:    
+                    break
+                    
+            if len(divisor) == 4:
+                res += sum(divisor)
+        return res  
